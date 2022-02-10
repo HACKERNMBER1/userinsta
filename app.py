a1a6c381ab07e7b9a41437472d7d402501e7eafe
HACKERNMBER1 <94354827+HACKERNMBER1@users.noreply.github.com>
2022-02-10 18:48:17 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import *
+
+from TSMRS import *
+
+import random
+
+a = Flask(__name__)
+
+@a.route('/')
+
+def khb():
+
+    k = 'kiuygfvbnjutrdcgytdrcugtr'
+
+    rr = random.choice(k)
+
+    rdr = random.choice(k)
+
+    mo = rr+rr+'_'+rdr+rr+rdr
+
+    s = check()
+
+    v = s.Telegram(mo)
+
+    if v == '404':
+
+        return {
+
+            'user':mo,
+
+            'result':'true'
+
+            'channel':'@lis_5_t'
+
+        }
+
+    else:
+
+        return {
+
+            'user':mo,
+
+            'result':'False'
+
+            'channel':'@lis_5_t'
+
+        }
+
+a.run()
