798104f38f554fdeb34420a80284383f8161954e
HACKERNMBER1 <94354827+HACKERNMBER1@users.noreply.github.com>
2022-02-10 19:07:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,55 @@ def khb():
 
             'result':'False'
 
-            'channel':'@lis_5_t'
+            'channel':'@lis_5_t'from flask import *
+
+from TSMRS import *
+
+import random
+
+a = Flask(__name__)
+
+@a.route('/')
+
+def khb():
+
+    k = 'kiuygfvbnjutrdcgytdrcugtr'
+
+    rr = random.choice(k)
+
+    rdr = random.choice(k)
+
+    mo = rr+rr+'_'+rdr+rr+rdr
+
+    s = check()
+
+    v = s.Telegram(mo)
+
+    if v == '404':
+
+        return {
+
+            'user':mo,
+
+            'result':'true'
+
+            
+
+        }
+
+    else:
+
+        return {
+
+            'user':mo,
+
+            'result':'False'
+
+            
+
+        }
+
+a.run()
 
         }
 
